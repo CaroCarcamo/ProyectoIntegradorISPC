874692a6367f92041cf1ca25ad454204f8f6ff0b
Willito83 <pwtorres10@gmail.com>
2022-08-12 00:39:41 -0300

    Subiendo resolución ejercicio 8 Base de Datos

---
+++ Valoracion_sql_ejercicio8.sql
@@ -0,0 +1,6 @@
+/*8. Insertar un nuevo registro en la tabla historial de un perro cuyo ID Perro es igual a 10.*/
+
+insert into historial ( Fecha, perro, Descripcion, Monto)
+values
+('2022-08-11', 10, 'control Vacunacion', 2000); 
+
